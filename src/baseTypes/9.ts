3ec1269c135c63a783dc34da5d353eacfc8b020e
X Mister <someinkognito@gmail.com>
2023-09-04 00:30:59 +0300

    homework done

---
+++ src/baseTypes/9.ts
@@ -0,0 +1,34 @@
+/*
+  Створіть новий тип даних, який підходить для цих двох об'єктів.
+*/
+
+type Data = {
+  title: string,
+  likes: number,
+  accounts: string[],
+  status: string,
+  details?: Details,
+}
+type Details = {
+  createAt: Date,
+  updateAt: Date,
+}
+const page1: Data = {
+  title: 'The awesome page',
+  likes: 100,
+  accounts: ['Max', 'Anton', 'Nikita'],
+  status: 'open',
+  details: {
+    createAt: new Date('2021-01-01'),
+    updateAt: new Date('2021-05-01'),
+  }
+}
+
+const page2: Data = {
+  title: 'Python or Js',
+  likes: 5,
+  accounts: ['Alex'],
+  status: 'close',
+}
+
+export {page1, page2};
